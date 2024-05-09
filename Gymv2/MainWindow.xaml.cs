18b0017abd10d858d17cf7f4e7c1785b5be62060
Socrate <145438031+3ricsilver@users.noreply.github.com>
2024-05-09 12:07:27 +0200

    Crash quand j'entre des données dans le mot de passe

--- Gymv2/MainWindow.xaml.cs
+++ Gymv2/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using Login;
+using System.Security;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +18,36 @@ namespace Gymv2
     /// </summary>
     public partial class MainWindow : Window
     {
+        public LoginUtilisateur Utilisateur { get; } = new LoginUtilisateur();
+
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = Utilisateur;
+        }
+        /*        private void txtPassword_PasswordChanged(object sender, RoutedEventArgs e) //cause des crashs
+                {
+                    Utilisateur.MotDePasse = new SecureString();
+                    foreach (char c in txtPassword.Password)
+                    {
+                        Utilisateur.MotDePasse.AppendChar(c);
+                    }
+
+                }*/
+
+        private void txtPassword_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            Utilisateur.MotDePasse = new SecureString();
+            foreach (char c in txtPassword.Password)
+            {
+                Utilisateur.MotDePasse.AppendChar(c);
+            }
+  
+        }
+
+        private void btnAfficherUtilisateur_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Utilisateur: {Utilisateur.Login}\nMot de passe: {Utilisateur.MotDePasse}");
         }
     }
 }
